a8085250aeeb85b72cd4a6bcb03750750ddade7b
GuanYu914 <aa774281@gmail.com>
2018-03-22 22:42:57 +0800

    rename define name and rewrite function SevenSegDisplay

--- display.c
+++ display.c
@@ -5,19 +5,19 @@
 void HC595Init()
 {
    //make the SER, SRCLK, RCLK lines as output
-   HC595_DDR|=((1<<HC595_SRCLK)|(1<<HC595_RCLK)|(1<<HC595_SER));
+   DISPLAY_DDR|=((1<<HC595_SRCLK)|(1<<HC595_RCLK)|(1<<HC595_SER));
 }
 
 void HC595Pulse()
 {
-   HC595_PORT|=(1<<HC595_SRCLK);    // set SRCLK line as high
-   HC595_PORT&=(~(1<<HC595_SRCLK)); // set SRCLK line as low
+   DISPLAY_PORT|=(1<<HC595_SRCLK);    // set SRCLK line as high
+   DISPLAY_PORT&=(~(1<<HC595_SRCLK)); // set SRCLK line as low
 }
 
 void HC595Latch()
 {
-   HC595_PORT|=(1<<HC595_RCLK);    // set RCLK line as high
-   HC595_PORT&=(~(1<<HC595_RCLK)); // set RCLK line as low
+   DISPLAY_PORT|=(1<<HC595_RCLK);    // set RCLK line as high
+   DISPLAY_PORT&=(~(1<<HC595_RCLK)); // set RCLK line as low
 }
 
 void HC595Write(uint8_t data)
@@ -43,9 +43,48 @@ void HC595Write(uint8_t data)
    HC595Latch();
 }
 
-void SevenSegment(uint8_t num)
+void SevenSegDisplay(uint8_t num)
 {
 	HC595Init();
-	HC595Write(num);
+	if(num == 9)
+	{
+		HC595Write(SEG_9);
+	}
+	if(num == 8)
+	{
+		HC595Write(SEG_8);
+	}
+	if(num == 7)
+	{
+		HC595Write(SEG_7);
+	}
+	if(num == 6)
+	{
+		HC595Write(SEG_6);
+	}
+	if(num == 5)
+	{
+		HC595Write(SEG_5);
+	}
+	if(num == 4)
+	{
+		HC595Write(SEG_4);
+	}
+	if(num == 3)
+	{
+		HC595Write(SEG_3);
+	}
+	if(num == 2)
+	{
+		HC595Write(SEG_2);
+	}
+	if(num == 1)
+	{
+		HC595Write(SEG_1);
+	}
+	if(num == 0)
+	{
+		HC595Write(SEG_0);
+	}
 }
 
