1aabb3dfd415e49306a1f2d0b01a6440c3fe8eaa
GuanYu914 <aa774281@gmail.com>
2018-03-31 22:46:16 +0800

    modified ISR function and add RTC code.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include "display.h"
-#define delay_time 10
+#include <display.h>
+#include <clock.h>
 
 // force to initialize this array as volatile type
 volatile uint8_t digit[4];
@@ -23,10 +23,8 @@ void FourSevenSegDisplay(uint16_t num)
 	}
 }
 
-int main(void)
-{	
-	uint16_t i;
-
+void TimerInit()
+{
 	// prescaler = FCPU / 256
 	TCCR0B|=(1<<CS02);
 	
@@ -41,19 +39,34 @@ int main(void)
 	
 	// enable global interrupt
 	sei();
+}
 
-	//FourSevenSegDisplay(9243);
+int main(void)
+{
+	uint8_t Temp_min = 0, Temp_hour = 0, Temp_temp = 0;
+
+	// SetHour(23);
+	// SetMinute(7);
+	// SetSecond(10);
+	HC595Init();	
+	TimerInit();
+	ClockInit();
+	
 	while(1)
-	{
-		FourSevenSegDisplay(9487);
-		//Wait();
-		/*
-		for(i = 0; i < 10000; i++)
-		{
-			FourSevenSegDisplay(i);
-			_delay_ms(10);
-		}
-		*/			
+	{			
+		Temp_min = GetMinute();
+		Temp_hour = GetHour();
+		digit[0] = Temp_min%10;
+		digit[1] = Temp_min/10;
+		digit[2] = Temp_hour%10;
+		digit[3] = Temp_hour/10;
+		_delay_ms(5000);
+		Temp_temp = GetTemp();
+		digit[0] = Temp_temp%10;
+		digit[1] = Temp_temp/10;
+		digit[2] = 0;
+		digit[3] = 0;	
+		_delay_ms(5000);
 	}
 	return 0;
 }
@@ -62,33 +75,36 @@ ISR(TIMER0_OVF_vect)
 {
 	static uint8_t i = 0;
 	
-	// turn off all digit switch
-	DISPLAY_PORT&=(~(1<<BUT_DIGIT0));
-	DISPLAY_PORT&=(~(1<<BUT_DIGIT1));
-	DISPLAY_PORT&=(~(1<<BUT_DIGIT2));
-	DISPLAY_PORT&=(~(1<<BUT_DIGIT3));
+	if(i == 3)
+	{
+		i = 0;
+	}else
+	{
+		i++;
+	}
 	
-	// display separate digit number alternately
-	SevenSegDisplay(digit[i]);
+	// turn off all digit switch
+	DISPLAY_PORT&=(~(1<<BUT_DIGIT3));
+	DISPLAY_PORT&=(~(1<<BUT_DIGIT2));
+	DISPLAY_PORT&=(~(1<<BUT_DIGIT1));
+	DISPLAY_PORT&=(~(1<<BUT_DIGIT0));
+
 	if(i == 3)
 	{
 		DISPLAY_PORT|=(1<<BUT_DIGIT3);
-		i = 0;
-	}else 
-	{
-		if(i == 0)
-		{	
-			DISPLAY_PORT|=(1<<BUT_DIGIT0);
-		}
-		if(i == 1)
-		{
-			DISPLAY_PORT|=(1<<BUT_DIGIT1);
-		}
-		if(i == 2)
-		{
-			DISPLAY_PORT|=(1<<BUT_DIGIT2);
-		}
-		i++;
 	}
+	if(i == 2)
+	{
+		DISPLAY_PORT|=(1<<BUT_DIGIT2);
+	}
+	if(i == 1)
+	{
+		DISPLAY_PORT|=(1<<BUT_DIGIT1);
+	}
+	if(i == 0)
+	{
+		DISPLAY_PORT|=(1<<BUT_DIGIT0);
+	}
+	SevenSegDisplay(digit[i]);
 }
 
