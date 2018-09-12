51e388dc5637c0546ea91f54db6a031d6570523f
GuanYu914 <aa774281@gmail.com>
2018-09-12 15:35:22 +0800

    add more descriptions for codes.

--- src/button.c
+++ src/button.c
@@ -1,21 +1,23 @@
 #include "button.h"
 
+// Use pull-down 
 // initialize pull up resistor to the pin 
+// Don't set DDRD register to input(0) because it will be zero after reset.
 void ButtonInit()
 {
 	BUTTON_PORT |= BUTTON_MASK;	
 }
 
-// solve push button debounce issue
+// solve push_button's debounce issue
 uint8_t ButtonDebounce()
 {
-	// counter for number of equal states
+	// count for number of equal states
 	static uint8_t count = 0;
 	// keep track of current (debounced) state
 	static uint8_t button_state = 0;
 	// set 1 if check button is debounced and changed 
 	uint8_t button_down = 0;
-	// check if button is high or low 
+	// check if button is high or low  
 	uint8_t current_state = (~BUTTON_PIN & BUTTON_MASK) != 0;
 	// if state is changed
 	if(current_state != button_state)
